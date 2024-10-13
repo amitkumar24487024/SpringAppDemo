57133611bc64b164aa41751ba343e837f39e36b9
Amit Kumar Singh <61904992+amitkumar24487024@users.noreply.github.com>
2024-10-13 17:35:18 +0000

    adding new enhacements

--- springappdemo/src/main/java/com/example/springappdemo/controller/HomeController.java
+++ springappdemo/src/main/java/com/example/springappdemo/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.example.springappdemo.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springappdemo.pojos.Student;
 
 @RestController
 public class HomeController {
@@ -9,5 +13,12 @@ public class HomeController {
     @GetMapping("hello")
     public String sayHello(){
         return "Hello";
+    }
+
+    
+    @GetMapping("students")
+    public List<Student> getAllStudents(){
+        return List.of(new Student(1L,"Amit","amit@gmail.com"),
+        new Student(2L,"Alisha","alisha@gmail.com"));
     }
 }
---
+++ springappdemo/src/main/java/com/example/springappdemo/pojos/Student.java
@@ -0,0 +1,37 @@
+package com.example.springappdemo.pojos;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+
+    public Student(){}
+    public Student(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", name=" + name + ", email=" + email + "]";
+    }
+
+}
