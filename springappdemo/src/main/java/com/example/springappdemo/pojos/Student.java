4398d4f11aae72772afa32405e1f303f6211e7bc
Amit Kumar Singh <61904992+amitkumar24487024@users.noreply.github.com>
2024-10-13 18:01:22 +0000

    using lombok

--- springappdemo/src/main/java/com/example/springappdemo/pojos/Student.java
+++ springappdemo/src/main/java/com/example/springappdemo/pojos/Student.java
@@ -1,37 +1,21 @@
 package com.example.springappdemo.pojos;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
 public class Student {
     private Long id;
     private String name;
     private String email;
-
-    public Student(){}
     public Student(Long id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public Long getId() {
-        return id;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    @Override
-    public String toString() {
-        return "Student [id=" + id + ", name=" + name + ", email=" + email + "]";
-    }
-
+    } 
+    
 }
+
