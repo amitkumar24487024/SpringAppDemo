630a5a94720ab4db2d86304a251026a030b7c070
Amit Kumar Singh <61904992+amitkumar24487024@users.noreply.github.com>
2024-10-13 17:21:07 +0000

    adding controller

---
+++ springappdemo/src/main/java/com/example/springappdemo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.springappdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping("hello")
+    public String sayHello(){
+        return "Hello";
+    }
+}
